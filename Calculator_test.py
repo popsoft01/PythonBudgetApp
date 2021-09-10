bbb6dcb48c34c75f3f86bc9cdb92b84f1d6e39eb
popsoft01 <popoolatunde52@gmail.com>
2021-09-10 21:59:42 +0100

    modify

--- Calculator_test.py
+++ Calculator_test.py
@@ -7,8 +7,11 @@ class MyTestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(5, Calculator.add(2, 3))  # add assertion here
 
-    def test_add2(self):
+    def test_add_with_negative_num(self):
         self.assertEqual(-1, Calculator.add(2, -3))
+
+    def test_add_with_two_zero(self):
+        self.assertEqual(0, Calculator.add(0, 0))
 
     def test_sub(self):
         self.assertEqual(3, Calculator.substract(5, 2))
