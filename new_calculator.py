53cc05dd6e6597e2f275305522969e04687e7bf3
popsoft01 <popoolatunde52@gmail.com>
2021-09-10 22:21:35 +0100

    modify

--- new_calculator.py
+++ new_calculator.py
@@ -1,3 +1,20 @@
-number_one = input('Enter first number')
-number_two = input('Enter the second number')
+def calculator(number, second_number, operator):
+    if operator == "+":
+        return number + second_number
+    elif operator == "-":
+        return number - second_number
+    elif operator == "*":
+        return number * second_number
+    elif operator == "/":
+        return number / second_number
+    elif operator == "%":
+        return number % second_number
+    else:
+        raise ValueError
+
+
+number_one = int(input('Enter first number'))
+number_two = int(input('Enter the second number'))
 operator = input('Enter the operator')
+result = calculator(number_one, number_two, operator)
+print(result)
