e19bff8db90f34afb4e2350ee32dae7ab7fa895e
popsoft01 <popoolatunde52@gmail.com>
2021-09-11 14:24:05 +0100

    modify

--- user_registeration.py
+++ user_registeration.py
@@ -1,12 +1,18 @@
-user_detail = {}
+
+class user_registration:
+    def __init__(self, user_email, user_username, user_password):
+        self.user_email = user_email
+        self.user_username = user_username
+        self.user_password = user_password
+        self.user_detail = {}
 
 
-def registration(new_email, new_username, new_password):
-    if len(new_email) >= 10 and len(new_username) >= 5 and len(new_password):
-        user_detail[new_email] = new_email
-        print(user_detail)
-        # return (f"Registration successful \n your registration details are {new_email},"
-        #         f" {new_password}, and {new_password}")
+    # def registration(new_email, new_username, new_password):
+    #     if len(new_email) >= 10 and len(new_username) >= 5 and len(new_password):
+    #         user_detail[new_email] = new_email
+    # print(user_detil)a
+    # return (f"Registration successful \n your registration details are {new_email},"
+    #         f" {new_password}, and {new_password}")
 
 # def login(user_email, username, user_password):
 #     user_detail = registration(user_email, username, user_password)
