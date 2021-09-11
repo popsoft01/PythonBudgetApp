b84e129f5bc7def1a4e117c2a456a0774404a641
popsoft01 <popoolatunde52@gmail.com>
2021-09-11 18:51:53 +0100

    modify

--- user_authentication.py
+++ user_authentication.py
@@ -1,4 +1,4 @@
 from PythonBudgetApp import user_registeration
 
-def registration(self, new_email, new_username, new_password):
-    user_registeration.user.get_user_details(u)
+def registration(new_email, new_username, new_password):
+    user_registeration.user.set_user_details(user_details=)
